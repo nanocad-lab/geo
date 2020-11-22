23aac2fbbf8e39da0fc5f41cd96bfa47e5747f54
Tianmu Li <litianmu1995@g.ucla.edu>
2020-11-21 16:14:53 -0800

    Removed dump

--- torch_c/lltm-extension/setup.py
+++
@@ -1,5 +0,0 @@
-from setuptools import setup, Extension
-from torch.utils import cpp_extension
-
-setup(name='lltm+cpp', ext_modules=[cpp_extension.CppExtension('lltm_cpp', ['lltm.cpp'])], cmdclass={'build_ext': cpp_extension.BuildExtension})
-
